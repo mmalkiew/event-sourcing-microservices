748fcd8398f871c89f429219093b0536b69be578
mmalkiew <michal.malkiewicz@uniteam.pl>
2019-10-09 23:13:10 +0200

    account query service as eureka client

--- account-query/src/main/java/com/mmalkiew/example/esm/account/query/AccountQueryApplication.java
+++ account-query/src/main/java/com/mmalkiew/example/esm/account/query/AccountQueryApplication.java
@@ -2,8 +2,10 @@ package com.mmalkiew.example.esm.account.query;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountQueryApplication {
 
     public static void main(String[] args) {
